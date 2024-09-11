a9b3e952534031efec4e63cf9bf32360caeff6ab
free999enigma <free999enigma@gmail.com>
2024-09-11 18:43:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ dotenv.config();
 
 const app = express();
 
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 const config = {
